9b1272e1b3811a9c6485966aa0b660e62777e50e
Will Boyd <will.boyd@abwlabs.com>
2019-09-20 18:32:29 -0500

    Added comment

--- deploy/start.sh
+++ deploy/start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-# handles startup on the docker container
+# handles startup on the docker container. see phusion/baseimage
 
 gunicorn --bind=0.0.0.0 --workers=2 foundry_backend.wsgi:application
