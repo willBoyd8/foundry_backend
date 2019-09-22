6d779ee5324d6c2017b841f362edf6ec9c198899
Will Boyd <will.boyd@abwlabs.com>
2019-09-21 21:36:17 -0500

    Added better secret key to start.sh

--- deploy/start.sh
+++ deploy/start.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 # handles startup on the docker container. see phusion/baseimage
 
+# Set a suitably random secret key
+FOUNDRY_SECRET_KEY=$(openssl rand -base64 100)
+
+# Start foundry
 gunicorn --bind=0.0.0.0 --workers=2 foundry_backend.wsgi:application
