0c6a0cae137f987c5af7d6f284caabf13ee5734d
Will Boyd <will.boyd@abwlabs.com>
2019-09-26 18:56:48 -0500

    added more stuff

--- foundry_backend/api/views.py
+++ foundry_backend/api/views.py
@@ -3,10 +3,41 @@ from rest_framework import viewsets
 from . import serializers
 
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API Endpoint for user creation and management
+    """
+    queryset = db_models.User.objects.all()
+    serializer_class = serializers.UserSerializer
+
+
 class AgencyViewSet(viewsets.ModelViewSet):
     """
     API Endpoint for Foundry Agencies
     """
     queryset = db_models.Agency.objects.all()
     serializer_class = serializers.AgencySerializer
+
+
+class MLSNumberViewSet(viewsets.ModelViewSet):
+    """
+    API Endpoint for Foundry Agencies
+    """
+    queryset = db_models.MLSNumber.objects.all()
+    serializer_class = serializers.MLSNumberSerializer
+
+
+class RealtorUserViewSet(viewsets.ModelViewSet):
+    """
+    API Endpoint for Foundry Agencies
+    """
+    queryset = db_models.Realtor.objects.all()
+    serializer_class = serializers.RealtorUserSerializer
+
+
+class ListingViewSet(viewsets.ModelViewSet):
+    """
+    API Endpoint for listings
+    """
+    queryset = db_models.Listing.objects.all()
+    serializer_class = serializers.ListingSerializer
