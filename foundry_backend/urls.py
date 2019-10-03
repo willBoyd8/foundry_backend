ed84e5c0d8b659b8261932a20e637af52843762e
Will Boyd <will.boyd@abwlabs.com>
2019-10-03 13:14:28 -0500

    Added token authorization

--- foundry_backend/urls.py
+++ foundry_backend/urls.py
@@ -17,8 +17,27 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
 from foundry_backend.api.urls import urlpatterns as api_urls
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Foundry MLS",
+        default_version='v1',
+        description="The Backend for Foundry",
+        terms_of_service="http://www.placekitten.com/400/400",
+        contact=openapi.Contact(email="foundry@abwlabs.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,)
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    url(r'^api/v1/', include(api_urls))
+    url(r'^api/v1/', include(api_urls)),
+    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
